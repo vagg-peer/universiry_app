99d2e87690e8529e559c081a75e27b8fd350cb40
vagg <vagg.karanikolos@gmail.com>
2025-02-14 22:57:12 +0200

    final fixes and refactors

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -9,6 +9,7 @@ use App\Service\StudentService;
 use App\Utils\StudentHelper;
 use Symfony\Component\HttpFoundation\Request;
 use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class StudentController extends AbstractController
 {
@@ -22,19 +23,28 @@ class StudentController extends AbstractController
     #[Route('/student', name: 'student_dashboard')]
     public function index(Request $request, PaginatorInterface $paginator): Response
     {
-        //get student dto
-        $studentDTO = $this->studentService->getStudentUserById($this->getUser()->getId());//ignore error from php Intelephense 
-        //get student grades
-        $gradesDTOs = $studentDTO->getGrades();
-        //paginate
-        $gradesDTOs = $paginator->paginate($gradesDTOs, $request->query->getInt('page', 1), 5);
-        //calculate semester
-        $semester = StudentHelper::calculateStudentSemester($studentDTO->getStartOfStudies());
+        try{
+            //get student dto
+            $studentDTO = $this->studentService->getStudentUserById($this->getUser()->getId());//ignore error from php Intelephense 
+            //get student grades
+            $gradesDTOs = $studentDTO->getGrades();
+            //paginate
+            $gradesDTOs = $paginator->paginate($gradesDTOs, $request->query->getInt('page', 1), 5);
+            //calculate semester
+            $semester = StudentHelper::calculateStudentSemester($studentDTO->getStartOfStudies());
+
+        }  catch (\Exception $e) {
+            $this->addFlash('error', 'An unexpected error occurred.');
+            $referer = $request->headers->get('referer');
+            return $referer ? new RedirectResponse($referer) : $this->redirectToRoute('app_login');
+        }
+
         return $this->render('student/index.html.twig', [
             'student' => $studentDTO,
             'grades' => $gradesDTOs,
             'semester' => $semester,
             'homePath' => '/student'
         ]);
+
     }
 }
--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -8,6 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 use App\Service\TeacherService;
 use Symfony\Component\HttpFoundation\Request;
 use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class TeacherController extends AbstractController
 {
@@ -21,10 +22,15 @@ class TeacherController extends AbstractController
     #[Route('/teacher', name: 'teacher_dashboard')]
     public function index(Request $request, PaginatorInterface $paginator): Response
     {
-        $teacherDTO = $this->teacherService->getTeacherUserById($this->getUser()->getId());//ignore error from php Intelephense 
-        $lessonDTOs = $teacherDTO->getLessons();
-        $lessonDTOs = $paginator->paginate($lessonDTOs, $request->query->getInt('page', 1), 5);
-        // dd($lessonDTOs);
+        try{
+            $teacherDTO = $this->teacherService->getTeacherUserById($this->getUser()->getId());//ignore error from php Intelephense 
+            $lessonDTOs = $teacherDTO->getLessons();
+            $lessonDTOs = $paginator->paginate($lessonDTOs, $request->query->getInt('page', 1), 5);
+        }  catch (\Exception $e) {
+            $this->addFlash('error', 'An unexpected error occurred.');
+            $referer = $request->headers->get('referer');
+            return $referer ? new RedirectResponse($referer) : $this->redirectToRoute('app_login');
+        }
         return $this->render('teacher/index.html.twig', [
             'teacher' => $teacherDTO,
             'lessons' => $lessonDTOs,
